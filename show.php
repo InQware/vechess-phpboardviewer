527fe8cbf4ebf174ca197449d9ebbfc3460d1cf3
root <root@qware.space>
2020-08-06 10:23:38 +0000

    empty board created

---
+++ show.php
@@ -0,0 +1,17 @@
+<?php
+echo "<link rel='stylesheet' href='style.css'>";
+echo "<table rows=8 cols=8 border=1>";
+$ltr = Array(1 => 'A', 2 => 'B', 3 => 'C', 4 => 'D', 5 => 'E', 6 => 'F', 7 => 'G', 8 => 'H');
+for ($row = 8; $row > 0; $row--) {
+	echo "<tr id=".$row.">";
+	foreach ($ltr as $key => $r) {
+		echo "<td id='".$r.$row."'"; 
+		if ((($row%2 == 1) && ($key%2 == 1)) || (($row%2 == 0) && ($key%2 == 0)))
+			echo "class='bl'>";
+		else
+			echo "class='wh'>";
+		}
+	echo "</tr>";
+}
+echo "</table>";
+?>
